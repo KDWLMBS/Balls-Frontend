70d4a9f6f56cf3ff71a76201223ecb9b3ba22aba
Wolfgang S <wolfgang.98@hotmail.com>
2018-02-24 13:54:02 +0100

    removed vuex from main.js, shouldnt be there

--- BallWeb/src/main.js
+++ BallWeb/src/main.js
@@ -1,12 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Vuex from 'vuex'
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
-Vue.use(Vuex)
 
 /* eslint-disable no-new */
 new Vue({
@@ -15,5 +13,3 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
-
-
